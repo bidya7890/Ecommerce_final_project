c1120a5a55e32f5c2e2f235ca91257a056f6ba00
bidya7890 <bidya7890@gmail.com>
2024-11-07 22:38:06 +0530

    added urls.py in app

--- ecommerceproject/ecommerceapp/urls.py
+++ ecommerceproject/ecommerceapp/urls.py
@@ -1,14 +1,29 @@
-from django.conf import settings
-from django.contrib import admin
-from django.conf.urls.static import static
-from django.urls import path
-from rest_framework import routers
+# from django.urls import path, include
+# from rest_framework.routers import DefaultRouter
+# from . import views
 
-router = routers.DefaultRouters()
+# router = DefaultRouter()
+# router.register(r'users', views.UserViewSet)
+# router.register(r'products', views.ProductViewSet)
+# router.register(r'cart', views.CartViewSet)
+# router.register(r'orders', views.OrderViewSet)
 
+# urlpatterns = [
+#     path('', include(router.urls)),
+# ]
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import ProductViewSet, CartViewSet, OrderViewSet, UserViewSet, UserCreateViewSet, LoginView
 
-
+router = DefaultRouter()
+router.register(r'users', UserViewSet, basename='user')
+router.register(r'register', UserCreateViewSet, basename='user-create')
+router.register(r'products', ProductViewSet)
+router.register(r'cart', CartViewSet, basename='cart')
+router.register(r'orders', OrderViewSet)
 
 urlpatterns = [
-    # path(),
+    path('', include(router.urls)),
+    path('login/', LoginView.as_view(), name='login'), 
+    #path('api/cart', CartViewSet.as_view())
 ]
